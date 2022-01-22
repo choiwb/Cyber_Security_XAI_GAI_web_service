013690a60ff3b90c23588c5bd979243633de902b
Woobin Choi <khuam1216@gmail.com>
2022-01-23 08:31:43 +0900

    commit

--- setting.py
+++ setting.py
@@ -7,5 +7,5 @@ SERVER_IP = '127.0.0.1'
 PORT = 17171
 
 
-IPS_model_path = '/Users/choiwb/Python_projects/이글루시큐리티_DSS_표준모델_Web_API/DSS_IPS_flask_server/saved_model/DSS_IPS_LightGBM.pkl'
+IPS_model_path = 'YOUR OWN PRE TRAINED MODEL PATH!!!!!!!!'
 IPS_model = pickle.load(open(IPS_model_path, 'rb'))
