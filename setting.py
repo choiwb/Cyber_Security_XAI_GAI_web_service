3aeec2a51408da7000b6ec66097153f40b1f6126
Woobin Choi <khuam1216@gmail.com>
2022-11-27 14:32:05 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -2,42 +2,77 @@
 
 import pickle
 import psycopg2 as pg2
-from DSS_IPS_shap_explainer_save import bert_predict
 import torch
-from transformers import AutoTokenizer, BertTokenizer, pipeline
 import numpy as np
 import scipy as sp
+from transformers import AutoTokenizer
+from transformers import pipeline, AutoTokenizer
+import shap
 
-SERVER_IP = '127.0.0.1'
-PORT = 17171 
 
-IPS_model_path = 'YOUR OWN PRE TRAINED MODEL PATH!!!!!!!!'
-IPS_explainer_path = 'YOUR OWN SHAP EXPLAINER (SQL feature) PATH!!!!!!!!'
-IPS_text_model_path = 'YOUR OWN TEXT FEATURE MODEL PATH!!!!!!!!'
-IPS_text_explainer_path = 'YOUR OWN SHAP EXPLAINER (Text feature) PATH!!!!!!!!'
-IPS_total_model_path = ''
-IPS_total_explainer_path = ''
+SERVER_IP = 'SERVER IP'
+PORT = 17171 # local
 
-IPS_model = pickle.load(open(IPS_model_path, 'rb'))
-IPS_explainer = pickle.load(open(IPS_explainer_path, 'rb'))
+
+# 절대 경로
+IPS_text_model_path = 'MODEL DIR !!!!!!!'
+IPS_total_model_path =  'MODEL DIR !!!!!!!'
+
+IPS_text_explainer_path = 'MODEL DIR !!!!!!!'
+IPS_total_explainer_path = 'MODEL DIR !!!!!!!'
+
+IPS_pytorch_bert_model_path = 'MODEL DIR !!!!!!!'
+# IPS_pytorch_bert_explainer_path = 'MODEL DIR !!!!!!!'
+
+
 IPS_text_model = pickle.load(open(IPS_text_model_path, 'rb'))
-IPS_text_explainer = pickle.load(open(IPS_text_explainer_path, 'rb'))
 IPS_total_model = pickle.load(open(IPS_total_model_path, 'rb'))
+
+IPS_text_explainer = pickle.load(open(IPS_text_explainer_path, 'rb'))
 IPS_total_explainer = pickle.load(open(IPS_total_explainer_path, 'rb'))
 
-IPS_pytorch_bert_model_path = ''
-IPS_pytorch_bert_explainer_path = ''
+IPS_pytorch_bert_model = torch.load(IPS_pytorch_bert_model_path)
+# IPS_pytorch_bert_explainer = torch.load(IPS_pytorch_bert_explainer_path)
 
-IPS_pytorch_bert_model = pickle.load(open(IPS_pytorch_bert_model_path, 'rb'))
-IPS_pytorch_bert_explainer = pickle.load(open(IPS_pytorch_bert_explainer_path, 'rb'))
+model_checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+
+print('BERT 전이학습 모델 평가: ', IPS_pytorch_bert_model.eval())
+
+
+device = torch.device('mps')
+# device = torch.device('cpu')
+
+# define a prediction function
+def bert_predict(x):
+    tv = torch.tensor([tokenizer.encode(v, padding='max_length', max_length=100, truncation=True) for v in x]).to(device)
+
+    #outputs = model(tv)[0].detach().cpu().numpy()
+    outputs = IPS_pytorch_bert_model(tv)[0].detach().cpu().numpy()
+
+    scores = (np.exp(outputs).T / np.exp(outputs).sum(-1)).T
+    val = sp.special.logit(scores[:,1]) # use one vs rest logit units
+    # val = shap_logit(val)
+    
+    return val
+
+bert_pipe = pipeline(task = "text-classification",
+                model = IPS_pytorch_bert_model,
+                tokenizer = tokenizer
+                )
+
+bert_pipe.device = device
+bert_pipe.model.to(device)
+
+# build an explainer using a token masker
+IPS_pytorch_bert_explainer = shap.Explainer(bert_predict, tokenizer)
+
 
 
 # PostgreSQL - Payload 예측 DB 연동
-conn = pg2.connect('''host = ??  
-                    dbname =  ??
-                    user =  ??
-                    password = ??  
-                    port = ?? ''')
-
-
+conn = pg2.connect('''host = ?? 
+                    dbname = ?? 
+                    user = ?? 
+                    password = ?? 
+                    port = ??''')
 
