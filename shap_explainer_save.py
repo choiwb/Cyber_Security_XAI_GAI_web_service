6d5a309351ba9556f09f63cd195471f900f30dc8
Woobin Choi <khuam1216@gmail.com>
2023-04-15 13:06:58 +0900

    Create shap_explainer_save.py

---
+++ shap_explainer_save.py
@@ -0,0 +1,15 @@
+import shap
+from setting import *
+import os
+
+explainer_save_path = 'save_model'
+
+ips_explainer = shap.TreeExplainer(IPS_model)
+waf_explainer = shap.TreeExplainer(WAF_model)
+web_explainer = shap.TreeExplainer(WEB_model)
+
+
+pickle.dump(ips_explainer, open(os.path.join(explainer_save_path, 'DSS_IPS_LGB_explainer_20230313.pkl'), 'wb'))
+pickle.dump(waf_explainer, open(os.path.join(explainer_save_path, 'DSS_WAF_LGB_explainer_20230313.pkl'), 'wb'))
+pickle.dump(web_explainer, open(os.path.join(explainer_save_path, 'DSS_WEB_LGB_explainer_20230404.pkl'), 'wb'))
+
