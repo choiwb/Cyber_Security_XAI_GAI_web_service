1696e04ecba37b0960b2fbdc0b141cacbf919269
Woobin Choi <khuam1216@gmail.com>
2023-06-22 18:33:36 +0900

    Update shap_explainer_save.py

--- shap_explainer_save.py
+++ shap_explainer_save.py
@@ -10,6 +10,8 @@ web_explainer = shap.TreeExplainer(WEB_model)
 
 
 pickle.dump(ips_explainer, open(os.path.join(explainer_save_path, 'DSS_IPS_LGB_explainer_20230615.pkl'), 'wb'))
-pickle.dump(waf_explainer, open(os.path.join(explainer_save_path, 'DSS_WAF_LGB_explainer_20230615.pkl'), 'wb'))
+# pickle.dump(waf_explainer, open(os.path.join(explainer_save_path, 'DSS_WAF_LGB_explainer_20230615.pkl'), 'wb'))
+pickle.dump(waf_explainer, open(os.path.join(explainer_save_path, 'DSS_WAF_sql_tfidf_LGB_explainer_20230622.pkl'), 'wb'))
+
 pickle.dump(web_explainer, open(os.path.join(explainer_save_path, 'DSS_WEB_LGB_explainer_20230404.pkl'), 'wb'))
 
