ce44c7d675aafbcaa666a7bbafb088fa8a9c6140
Woobin Choi <khuam1216@gmail.com>
2023-06-28 20:59:43 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -120,7 +120,7 @@ WAF_DL_XAI = shap.Explainer(waf_bert_predict, masker)
 
 ####################################################################################
 # WEB 딥러닝 모델 호출
-WEB_DL_path = '/home/xai/xai_flask/save_model/WEB_DistilBERT_20230628'
+WEB_DL_path = 'save_model/WEB_DistilBERT_20230628'
 
 WEB_DL_model = AutoModelForSequenceClassification.from_pretrained(WEB_DL_path)
 WEB_DL_tokenizer = AutoTokenizer.from_pretrained(WEB_DL_path)
