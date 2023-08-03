025a3d6860b0da118baabeaeee0a09c50772d8bc
Woobin Choi <khuam1216@gmail.com>
2023-08-03 20:08:18 +0900

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -596,10 +596,7 @@ def shap_logit(x):
 # 시그니처 패턴 리스트 csv 호출 => 사용자 정의 & Web CGI 공격 & 패턴 블럭  sheet 참조! (단, Snort(사용자 패턴) 시트 제외!)
 sig_pattern_csv_path = 'save_model'
 df = pd.read_excel(os.path.join(sig_pattern_csv_path, 'signature_pattern_list_20230803.xlsx'))
-# print('@@@@@@@@@@@@@@')
-# 총 3435 개
-# print('시그니처 패턴 총 개수: ', df.shape[0])
-# print('@@@@@@@@@@@@@@')
+
 
 signature_list = df['탐지 패턴'].tolist()
 # 전체 시그니처 패턴 리스트 소문자화
