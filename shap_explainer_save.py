21dc3831416f44f55b6968fb2ae4494850d711f4
Woobin Choi <khuam1216@gmail.com>
2023-08-14 22:37:27 +0900

    Update shap_explainer_save.py

--- shap_explainer_save.py
+++ shap_explainer_save.py
@@ -9,9 +9,8 @@ waf_explainer = shap.TreeExplainer(WAF_model)
 web_explainer = shap.TreeExplainer(WEB_model)
 
 
-pickle.dump(ips_explainer, open(os.path.join(explainer_save_path, 'DSS_IPS_LGB_explainer_20230615.pkl'), 'wb'))
-# pickle.dump(waf_explainer, open(os.path.join(explainer_save_path, 'DSS_WAF_LGB_explainer_20230615.pkl'), 'wb'))
-pickle.dump(waf_explainer, open(os.path.join(explainer_save_path, 'DSS_WAF_sql_tfidf_LGB_explainer_20230622.pkl'), 'wb'))
+pickle.dump(ips_explainer, open(os.path.join(explainer_save_path, 'IPS_ML_XAI_20230817.pkl'), 'wb'))
+pickle.dump(waf_explainer, open(os.path.join(explainer_save_path, 'WAF_ML_XAI_20230817.pkl'), 'wb'))
+# pickle.dump(waf_explainer, open(os.path.join(explainer_save_path, 'DSS_WAF_sql_tfidf_LGB_explainer_20230622.pkl'), 'wb'))
 
-pickle.dump(web_explainer, open(os.path.join(explainer_save_path, 'DSS_WEB_LGB_explainer_20230713.pkl'), 'wb'))
-
+pickle.dump(web_explainer, open(os.path.join(explainer_save_path, 'WEB_ML_XAI_20230817.pkl'), 'wb'))
